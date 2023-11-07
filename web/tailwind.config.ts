71fa02474fee8a5d26dc18a4ff497d56b5d5cfac
MiroslavSapunar <psapunar@fi.uba.ar>
2023-11-07 16:52:19 -0300

    added basic brand style to TS config

--- web/tailwind.config.ts
+++ web/tailwind.config.ts
@@ -14,6 +14,19 @@ const config: Config = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
+    colors: {
+      primary_d: "#0f46f5",
+      primary_l: "#28c3fa",
+      alter_d: "#1e2346",
+      alter_l: "#003c82",
+      medium_tone_d: "#c3c5c9",
+      medium_tone_l: "#e4f3fc",
+      transparent: "#00000000",
+    },
+    fontFamily: {
+      'display': ['Open Sans Extrabold'],
+      'body': ['Open Sans'],
+    },
   },
   plugins: [],
 }
