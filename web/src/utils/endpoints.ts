70f9052b0bd93fabf86b4e904224804fbfb23b6a
MiroslavSapunar <psapunar@fi.uba.ar>
2023-12-13 12:36:37 -0300

    endpoint utils

---
+++ web/src/utils/endpoints.ts
@@ -0,0 +1,35 @@
+import qs from "qs";
+declare module 'qs';
+
+const strapi_url_front = process.env.NEXT_PUBLIC_STRAPI_URL_FRONT || 'http://localhost:1337'
+const strapi_url_back = process.env.STRAPI_URL_BACK || 'http://strapi:1337'
+
+export enum ETarget {
+    front,
+    back
+}
+
+export const endpoint = (destino: ETarget, url: string): string => {
+    if (!url)
+        return ""
+    switch (destino) {
+        case ETarget.back:
+            return `${strapi_url_back}${url}`
+        case ETarget.front:
+            return `${strapi_url_front}${url}`
+    }
+}
+
+export const queryHome = qs.stringify(
+    {
+        populate: ['logo_svg', 'categorias.navegaciones.navegacion', 'estilo_texto_categorias', 'boton_link.navegacion.navegacion']
+    },
+    {
+        encodeValuesOnly: true, // prettify URL
+    }
+);
+
+export const strapi_url = {
+    base: `/`,
+    navbar: `/api/nav-bar?${queryHome}`,
+}
